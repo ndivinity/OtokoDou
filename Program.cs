03d4c7b583abc715bfad0a5a38d05981e548f9d7
Akira Jeon <ndivinity69@gmail.com>
2025-03-06 12:52:33 -0500

    First implementation, does nothing.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Otoko;
+
+using System.Threading.Tasks;
+using Raylib_CsLo;
+
+class Dousa {
+    private static void Main(string[] argv) {
+        Raylib.InitWindow(1280, 720, "男らしい動作");
+        while(!Raylib.WindowShouldClose()) {
+            Raylib.ClearBackground(Raylib.GREEN);
+
+            Raylib.BeginDrawing();
+            Raylib.EndDrawing();
+        }
+        Raylib.CloseWindow();
+    }
+}
